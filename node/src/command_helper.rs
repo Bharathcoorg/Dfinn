4029425535b6d71b0f83aa5c97a29881ce179459
Bharathcoorg <40591495+Bharathcoorg@users.noreply.github.com>
2022-05-28 10:41:53 +0000

    Update command_helper.rs

--- node/src/command_helper.rs
+++ node/src/command_helper.rs
@@ -21,7 +21,7 @@
 
 use crate::service::{create_extrinsic, FullClient};
 
-use node_polkadex_runtime::SystemCall;
+use node_dfinn_runtime::SystemCall;
 use sc_cli::Result;
 use sp_inherents::{InherentData, InherentDataProvider};
 use sp_keyring::Sr25519Keyring;
