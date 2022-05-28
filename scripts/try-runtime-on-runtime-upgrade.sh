7625ddcf3880cb2bd608cd4f1ce5662c27a98ab5
Bharathcoorg <40591495+Bharathcoorg@users.noreply.github.com>
2022-05-28 10:34:24 +0000

    Update try-runtime-on-runtime-upgrade.sh

--- scripts/try-runtime-on-runtime-upgrade.sh
+++ scripts/try-runtime-on-runtime-upgrade.sh
@@ -1,1 +1,1 @@
-RUST_LOG=remote-ext=debug,runtime=trace,executor=trace,bags-list=trace ../target/release/polkadex-node try-runtime on-runtime-upgrade live --uri ws://localhost:9944
+RUST_LOG=remote-ext=debug,runtime=trace,executor=trace,bags-list=trace ../target/release/dfinn-node try-runtime on-runtime-upgrade live --uri ws://localhost:9944
