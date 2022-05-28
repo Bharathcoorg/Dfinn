e5673dc0b4bad725eb0dc321e13033c2934d5fc0
Bharathcoorg <40591495+Bharathcoorg@users.noreply.github.com>
2022-05-28 11:40:22 +0000

    Update constants.rs

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -21,9 +21,9 @@
 pub mod currency {
 	use dfinn_primitives::Balance;
 
-	pub const PDEX: Balance = 1000_000_000_000;
-	pub const UNITS: Balance = PDEX;
-	pub const DOLLARS: Balance = PDEX; // 1000_000_000_000
+	pub const FINN: Balance = 1000_000_000_000;
+	pub const UNITS: Balance = FINN;
+	pub const DOLLARS: Balance = FINN; // 1000_000_000_000
 	pub const CENTS: Balance = DOLLARS / 100; // 10_000_000_000
 	pub const MILLICENTS: Balance = CENTS / 1_000; // 1000_000_000
 
