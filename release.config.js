e0552f52fce54c8547bfa2a515d42853d0d083f8
Bharathcoorg <40591495+Bharathcoorg@users.noreply.github.com>
2022-05-28 10:11:37 +0000

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     branches: "main",
-    repositoryUrl: "https://github.com/Polkadex-Substrate/Polkadex",
+    repositoryUrl: "https://github.com/Bharathcoorg/Dfinn",
     plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/github']
 }
